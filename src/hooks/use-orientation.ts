5b434de91123d8598651c3a468eb14f1fc305caf
maxhaider <hello@maxhaider.com>
2022-05-15 18:43:09 +0200

    Added defaultOrientation option for useOrientation

--- src/hooks/use-orientation.ts
+++ src/hooks/use-orientation.ts
@@ -2,35 +2,61 @@ import { useMemo } from "react";
 import { StyleProp } from "react-native";
 import { Orientation } from "../types/orientation";
 
+interface IOrientationOptions {
+  style?: StyleProp<any>;
+  defaultOrientation?: Orientation;
+}
+
 export const useOrientation = (
   orientation: Orientation,
-  style: StyleProp<any> = {}
+  options: IOrientationOptions = {
+    style: {},
+    defaultOrientation: "up",
+  }
 ) =>
   useMemo(() => {
+    const { style, defaultOrientation } = options;
     const transform = style?.transform || [];
+
+    if (orientation === defaultOrientation) {
+      return style;
+    }
+
+    let defaultRotation = 0;
+
+    switch (defaultOrientation) {
+      case "up":
+        break;
+      case "left":
+        defaultRotation = 90;
+        break;
+      case "right":
+        defaultRotation = -90;
+        break;
+      case "down":
+        defaultRotation = -180;
+        break;
+    }
 
     switch (orientation) {
       case "up":
-        return style;
+        transform.push({ rotate: `${defaultRotation}deg` });
+        break;
       case "left":
-        transform.push({ rotate: "-90deg" });
-        return {
-          ...style,
-          transform,
-        };
+        transform.push({ rotate: `${-90 + defaultRotation}deg` });
+        break;
       case "right":
-        transform.push({ rotate: "90deg" });
-        return {
-          ...style,
-          transform,
-        };
+        transform.push({ rotate: `${90 + defaultRotation}deg` });
+        break;
       case "down":
-        transform.push({ rotate: "-180deg" });
-        return {
-          ...style,
-          transform,
-        };
+        transform.push({ rotate: `${180 + defaultRotation}deg` });
+        break;
       default:
         return style;
     }
-  }, [orientation, style]);
+
+    return {
+      ...style,
+      transform,
+    };
+  }, [orientation, options]);
